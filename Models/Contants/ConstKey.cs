6de5a2d53989d3d220b7a7cfc9f0e942459c75ab
Seyoon <psyoona@naver.com>
2023-09-03 22:34:17 +0900

    Ver 1.00.009 CommonDao 추가

---
+++ Models/Contants/ConstKey.cs
@@ -0,0 +1,7 @@
+﻿namespace YL.Models.Contants
+{
+	public class ConstKey
+	{
+		public static string CONNECTION_STRING { get; } = "CONNECTION_STRING";
+	}
+}
---
+++ Models/Daos/BaseCommonDao.cs
@@ -0,0 +1,15 @@
+﻿using YL.Models.Contants;
+using YL.Models.Functions;
+
+namespace YL.Models.Daos
+{
+	public class BaseCommonDao
+	{
+		protected string ConnectionString { get; set; }
+
+		protected string GetConnectionString()
+		{
+			return CustomConfig.AppSettings.GetConnectionString(ConstKey.CONNECTION_STRING);
+		}
+	}
+}
