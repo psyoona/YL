62046bdf57026806a1436beb639eb10ac3543ce3
Seyoon <psyoona@naver.com>
2023-09-03 21:56:04 +0900

    Ver 1.00.003 VersionHelper 추가

---
+++ Models/Functions/VersionHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace YL.Models.Functions
+{
+	public class VersionHelper
+	{
+		public static string GetApplicationVersion()
+		{
+			Version version = Assembly.GetExecutingAssembly().GetName().Version;
+
+			return $"{version.Major}.{version.Minor:D2}.{version.Build:D3}";
+		}
+	}
+}
