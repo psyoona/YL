a96a58e8d1a778f75617d58a4a7758252de2f59b
Seyoon <psyoona@naver.com>
2023-09-04 23:38:45 +0900

    Ver 1.01.001 iphone 더블탭 방지 기능 추가

--- wwwroot/js/Tetris/tetris.js
+++ wwwroot/js/Tetris/tetris.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
+	var lastTouchEnd = 0;
+	document.addEventListener('touchend', function (event) {
+		var now = (new Date()).getTime();
+		if (now - lastTouchEnd <= 300) {
+			event.preventDefault();
+		}
+		lastTouchEnd = now;
+	}, false);
+
 	let tetris = new Tetris();
 	tetris.init();
 
