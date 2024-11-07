5d5565680fbaa194d412e924228f113175f8bfc9
seyoon <psyoona@naver.com>
2024-11-07 23:24:04 +0900

    Ver 1.11.002 Test 기능 추가

---
+++ Controllers/Apis/TeamsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YL.Models.BusinessLogicLayers;
+using YL.Models.Dtos.Apis;
+
+namespace YL.Controllers.Apis
+{
+	[ApiController]
+	public class TeamsController : BaseApiController
+	{
+		private ILogger Logger { get; }
+
+		public TeamsController(IConfiguration configuration, ILogger<KakaoUserRequest> logger) : base(configuration)
+		{
+			this.Logger = logger;
+		}
+
+		[HttpGet]
+		[Route("GetTeamsMessage")]
+		public JsonResult GetTeamsMessage()
+		{
+			
+
+			return this.Json(new { test = "zxcvb" });
+		}
+	}
+}
