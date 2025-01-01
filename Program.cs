dbed8ef3ca03d6d40da5f1b228bfc37d3e5b67ed
Seyoon <psyoona@naver.com>
2025-01-01 23:06:48 +0900

    Ver 1.12.001 Change base page

--- Program.cs
+++ Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Laboratory}/{action=BaseConversion}/{id?}");
 
 app.Run();
